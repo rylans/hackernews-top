26e166a40156423b6eb1b0d5c46ff50700da4d5f
Rylan <rylans@gmail.com>
2014-10-10 15:54:53 -0700

    Fix unicode decode error

--- top.py
+++ top.py
@@ -6,6 +6,16 @@
 import urllib2
 import json
 
+output_file = "top.out"
+
+def write_stories(stories):
+  f = open(output_file, "w")
+  for story in stories:
+    story_string = story_to_string(story).encode('utf-8')
+    f.write(story_string)
+    f.write('\n')
+  f.close()
+
 def make_item_endpoint(item_id):
   return "https://hacker-news.firebaseio.com/v0/item/" + str(item_id) + ".json"
 
@@ -32,12 +42,18 @@ def get_item(item_id):
   title = story["title"]
   by = story["by"]
 
-  print story_to_string(story)
+  return story
 
 def main():
   article_list = get_top()
+  stories = []
 
   for i in article_list[:10]:
-    get_item(i)
+    story = get_item(i)
+    print story_to_string(story)
+    stories.append(story)
+
+  write_stories(stories)
+
 
 main()
