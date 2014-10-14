4c3106b198027cc8318ff9af9b1affd59df80d05
Rylan <rylans@gmail.com>
2014-10-14 15:24:14 -0700

    Put csv handling code into csv_io

---
+++ csv_io.py
@@ -0,0 +1,118 @@
+## csv_io.py
+##
+## Rylan Santinon
+
+from time import strftime
+import os
+
+class CsvIo:
+  def __init__(self):
+    self.ext_csv = ".csv"
+    self.users_dir = "users"
+    self.stories_dir = "stories"
+    self.ignore = ['all_users.csv']
+
+  def get_datetime(self):
+    return strftime("%Y-%m-%d")
+
+  def get_path(self, filename):
+    """Get the proper relative path for a yyyy-mm-dd filename
+
+    >>> CsvIo().get_path('2014-10-14.csv')
+    '2014/10/2014-10-14.csv'
+    """
+    parts = filename.split('-')
+    return os.path.join(parts[0],parts[1],filename)
+
+  def write_stories_csv(self, stories):
+    filepath = self.get_datetime() + self.ext_csv
+    fullpath = os.path.join(self.stories_dir, self.get_path(filepath))
+    f = open(fullpath, "w")
+    f.write("SCORE,TITLE,BY,URL\n")
+    for story in stories:
+      story_string = self.story_to_csv(story).encode('utf-8')
+      f.write(story_string)
+      f.write('\n')
+    f.close()
+
+  def write_users_csv(self, users):
+    filepath = self.get_datetime() + self.ext_csv
+    fullpath = os.path.join(self.users_dir,self.get_path(filepath))
+    f = open(fullpath, "w")
+    f.write("ID,KARMA,CREATED,SUBMISSIONS\n")
+    for user in users:
+      user_string = self.user_to_csv(user).encode('utf-8')
+      f.write(user_string)
+      f.write('\n')
+    f.close()
+
+  def story_to_csv(self, story):
+    #SCORE,TITLE,BY,URL
+    cols = []
+    cols.append(str(story["score"]))
+    cols.append(story["title"])
+    cols.append(story["by"])
+    cols.append(story["url"])
+    csv_cols = [self.remove_csv_chars(col) for col in cols]
+    return ','.join(csv_cols)
+
+  def user_to_csv(self, user):
+    #ID,KARMA,CREATED,SUBMISSIONS
+    cols = []
+    cols.append(user["id"])
+    cols.append(str(user["karma"]))
+    cols.append(str(user["created"]))
+    submitted = [str(s) for s in user["submitted"]]
+    cols.append(str(len(submitted)))
+    csv_cols = [self.remove_csv_chars(col) for col in cols]
+    return ','.join(csv_cols)
+
+  def remove_csv_chars(self, text):
+    return self.remove_commas(self.remove_quotes(text))
+
+  def remove_quotes(self, text):
+    return text.replace('"','')
+
+  def remove_commas(self, text):
+    return text.replace(',','')
+
+  def recursive_walk(self, directory):
+    file_list = []
+    for root, dirs, files in os.walk(directory):
+      for f in files:
+	if f not in self.ignore:
+	  file_list.append(os.path.join(root,f))
+    return file_list
+
+  def concat_users(self):
+    user_csvs = self.recursive_walk(self.users_dir)
+    user_lines = {}
+
+    for user_csv in user_csvs:
+      f = open(user_csv)
+      i = 0
+      for line in f.readlines():
+	if i == 0:
+	  i += 1
+	  continue
+	stripped_line = line.strip()
+	user_lines[stripped_line.split(',')[0]] = stripped_line
+	#TODO: Resolve duplicates by greatest submissions
+      f.close()
+
+    line_list = [user_lines[k] for k in user_lines.keys()]
+    sorted_users = sorted(line_list)
+
+    f = open(os.path.join(self.users_dir,'all_users.csv'), "w")
+    f.write("ID,KARMA,CREATED,SUBMISSIONS\n")
+    wrote = 0
+    for u in sorted_users:
+      wrote += 1
+      f.write(u)
+      f.write('\n')
+    f.close()
+    return wrote
+
+if __name__ == '__main__':
+  import doctest
+  doctest.testmod()
--- top.py
+++ top.py
@@ -4,147 +4,12 @@
 ##
 ## Rylan Santinon
 
-from time import strftime
 from api_connector import *
-import os
-
-ext_out = ".out"
-ext_csv = ".csv"
-
-users_dir = "users"
-stories_dir = "stories"
-
-
-def get_datetime():
-  return strftime("%Y-%m-%d")
-
-def get_path(filename):
-  #If file is 2014-10-14.csv then return 2014/10/2014-10-14.csv
-  parts = filename.split('-')
-  return os.path.join(parts[0],parts[1],filename)
-  
-def write_stories(stories):
-  filepath = get_datetime() + ext_out
-  fullpath = os.path.join(stories_dir,get_path(filepath))
-  f = open(fullpath, "w")
-  for story in stories:
-    story_string = story_to_string(story).encode('utf-8')
-    f.write(story_string)
-    f.write('\n')
-  f.close()
-
-def write_stories_csv(stories):
-  filepath = get_datetime() + ext_csv
-  fullpath = os.path.join(stories_dir,get_path(filepath))
-  f = open(fullpath, "w")
-  f.write("SCORE,TITLE,BY,URL\n")
-  for story in stories:
-    story_string = story_to_csv(story).encode('utf-8')
-    f.write(story_string)
-    f.write('\n')
-  f.close()
-
-def write_users_csv(users):
-  filepath = get_datetime() + ext_csv
-  fullpath = os.path.join(users_dir,get_path(filepath))
-  f = open(fullpath, "w")
-  f.write("ID,KARMA,CREATED,SUBMISSIONS\n")
-  for user in users:
-    user_string = user_to_csv(user).encode('utf-8')
-    f.write(user_string)
-    f.write('\n')
-  f.close()
-  
-def story_to_string(story):
-  try:
-    score = story["score"]
-    title = story["title"]
-    by = story["by"]
-  except KeyError as e:
-    print story
-    raise e
-
-  return "[" + str(score) + "] " + title + " (" + by + ")"
-
-def user_to_string(user):
-  name = user["id"]
-  karma = str(user["karma"])
-  created = str(user["created"])
-  submitted = [str(s) for s in user["submitted"]]
-  submissions = str(len(submitted))
-
-  return name + " (" + karma + ") <" + created + "> " + submissions
-
-def remove_csv_chars(text):
-  return remove_commas(remove_quotes(text))
-
-def remove_quotes(text):
-  return text.replace('"','')
-
-def remove_commas(text):
-  return text.replace(',','')
-
-def story_to_csv(story):
-  #SCORE,TITLE,BY,URL
-  cols = []
-  cols.append(str(story["score"]))
-  cols.append(story["title"])
-  cols.append(story["by"])
-  cols.append(story["url"])
-  csv_cols = [remove_csv_chars(col) for col in cols]
-  return ','.join(csv_cols)
-
-def user_to_csv(user):
-  #ID,KARMA,CREATED,SUBMISSIONS
-  cols = []
-  cols.append(user["id"])
-  cols.append(str(user["karma"]))
-  cols.append(str(user["created"]))
-  submitted = [str(s) for s in user["submitted"]]
-  cols.append(str(len(submitted)))
-  csv_cols = [remove_csv_chars(col) for col in cols]
-  return ','.join(csv_cols)
-
-def recursive_walk(directory):
-  IGNORE = ['all_users.csv']
-  file_list = []
-  for root, dirs, files in os.walk(directory):
-    for f in files:
-      if f not in IGNORE:
-        file_list.append(os.path.join(root,f))
-  return file_list
-
-def concat_users():
-  print "concat_users"
-
-  user_csvs = recursive_walk(users_dir)
-  user_lines = {}
-
-  for user_csv in user_csvs:
-    f = open(user_csv)
-    i = 0
-    for line in f.readlines():
-      if i == 0:
-        i += 1
-        continue
-      stripped_line = line.strip()
-      user_lines[stripped_line.split(',')[0]] = stripped_line
-      #TODO: Resolve duplicates by greatest submissions
-      
-    f.close()
-
-  line_list = [user_lines[k] for k in user_lines.keys()]
-  sorted_users = sorted(line_list)
-
-  f = open(os.path.join(users_dir,'all_users.csv'), "w")
-  f.write("ID,KARMA,CREATED,SUBMISSIONS\n")
-  for u in sorted_users:
-    f.write(u)
-    f.write('\n')
-  f.close()
+from csv_io import *
 
 def main():
   conn = ApiConnector()
+  csvio = CsvIo()
   article_list = conn.get_top()
   stories = []
 
@@ -153,13 +18,12 @@ def main():
       story = conn.get_item(i)
       if story.get("deleted"):
 	continue
-      print story_to_string(story)
+      print csvio.story_to_csv(story)
       stories.append(story)
     except NetworkError as e:
       print e
 
-  write_stories(stories)
-  write_stories_csv(stories)
+  csvio.write_stories_csv(stories)
 
   for story in stories:
     try:
@@ -176,8 +40,8 @@ def main():
     except NetworkError as e:
       print e
 
-  write_users_csv(users)
+  csvio.write_users_csv(users)
 
 if __name__ == '__main__':
   main()
-  concat_users()
+  CsvIo().concat_users()
