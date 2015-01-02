6484ef2f2962cac4c155626140b3acbbb402ce33
Rylan <rylans@gmail.com>
2015-01-02 14:07:46 -0800

    Unit tests in test folder

---
+++ tests/test_hnapi.py
@@ -0,0 +1,19 @@
+"""
+Tests
+"""
+import unittest
+
+from hnapi.connectors.api_connector import ApiConnector
+
+class HnapiTest(unittest.TestCase):
+    """
+    Test hnapi
+    """
+    def test_get_item(self):
+        """
+        Test item retrieval and 'by' field
+        """
+        con = ApiConnector()
+        item = con.get_item(8863)
+        byline = item.get('by')
+        self.assertEqual(byline, u'dhouston')
