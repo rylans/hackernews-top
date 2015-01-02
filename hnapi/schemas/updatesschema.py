acc31eba68d28d288037a306a8659396cf501497
Rylan <rylans@gmail.com>
2015-01-02 13:29:02 -0800

    Make schema for /updates endpoint

---
+++ hnapi/schemas/updatesschema.py
@@ -0,0 +1,54 @@
+"""
+Updates Schema
+"""
+
+from .schema import Schema
+from .field import Field
+
+class UpdatesSchema(Schema):
+    """Updates Schema (Items, Profiles)
+
+    Examples
+    --------
+
+    >>> UpdatesSchema().get_fields()[0].get_name()
+    'items'
+    """
+    def __init__(self):
+        self.fields = []
+        self.fields.append(Field("items"))
+        self.fields.append(Field("profiles"))
+
+    def get_fields(self):
+        return self.fields
+
+    def has_field(self, name):
+        """Return True iff this schema contains a field named 'name'
+
+        Parameters
+        ----------
+        name : str
+            Name of the field if it exists
+
+        Returns
+        -------
+        bool
+            True if this schema contains `name`, False otherwise
+
+        Examples
+        --------
+
+        >>> UpdatesSchema().has_field('items')
+        True
+
+        >>> UpdatesSchema().has_field('zzz')
+        False
+        """
+        return any([f.get_name() == name for f in self.fields])
+
+    def __repr__(self):
+        return "UpdatesSchema(fields=%r)" % self.fields
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
