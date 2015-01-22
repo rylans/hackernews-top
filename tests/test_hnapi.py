8ebeadd99b0f3a997965df0bba23013fbf1ffa0d
Rylan <rylans@gmail.com>
2015-01-22 09:57:07 -0800

    Add test for get_top

--- tests/test_hnapi.py
+++ tests/test_hnapi.py
@@ -7,6 +7,7 @@ import unittest
 
 from hnapi.connectors.api_connector import ApiConnector
 
+#pylint: disable=too-many-public-methods
 class HnapiTest(unittest.TestCase):
     """
     Test hnapi
@@ -50,6 +51,20 @@ class HnapiTest(unittest.TestCase):
         item = con.get_item(updates.get('items')[0])
         year_2001 = 1000000000
         self.assertTrue(item.get('time') > year_2001)
+
+    def test_get_top(self):
+        """
+        Test retrieval of first and last items from /top endpoint
+        """
+        con = ApiConnector()
+        top = con.get_top()
+        self.assertTrue(len(top) == 100)
+
+        item_0 = con.get_item(top[0])
+        self.assertTrue(item_0.get('id') > 0)
+
+        item_100 = con.get_item(top[-1])
+        self.assertTrue(item_100.get('id') > 0)
 
 if __name__ == '__main__':
     unittest.main()
