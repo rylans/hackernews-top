0bff1e01467453796893140aef6385187bc1fda5
Rylan <rylans@gmail.com>
2016-03-31 12:52:53 -0700

    Change module name in setup

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python
 """setup.py"""
-#pylint: disable=bad-continuation
+# pylint: disable=bad-continuation
 
 from setuptools import setup, find_packages
 
-setup(name='HN API',
-    version='0.0.4',
-    description='Python wrapper over HackerNews Firebase API',
-    author='Rylan Santinon',
-    url='https://github.com/rylans/hackernews-top',
-    packages=find_packages())
+setup(name='hntop',
+      version='0.0.5',
+      description='Python wrapper over HackerNews Firebase API',
+      author='Rylan Santinon',
+      author_email='rylans@gmail.com',
+      url='https://github.com/rylans/hackernews-top',
+      keywords=['hn', 'hackernews', 'hnapi', 'hackernews top'],
+      packages=find_packages())
